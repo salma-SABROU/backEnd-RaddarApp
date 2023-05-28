9b006253295ca0486028f1a4cdfd16806cda2cda
salma-SABROU <129564311+salma-SABROU@users.noreply.github.com>
2023-05-28 02:55:13 +0100

    Add files via upload

---
+++ radar-getway/src/test/java/org/sdia/radargetway/RadarGetwayApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sdia.radargetway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RadarGetwayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
